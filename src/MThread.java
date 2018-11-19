7046340d667039cf3bd9565634a3734e7b425433
manoj <>
2018-11-19 17:30:49 +0530

    19-11-18

--- src/MThread.java
+++ src/MThread.java
@@ -8,6 +8,7 @@ try
 	System.out.println(s+"=>Statement-1");
 	if(s.equals("BB"))
 	{
+
 		System.in.read();
 	}
 	System.out.println(s+"=>Statement-2");
@@ -18,17 +19,72 @@ catch(Exception e){}
 }
 public static void main(String args[]) throws Exception
 {
+
+	Thread tmp=Thread.currentThread();
+	System.out.println(tmp.getName());
+	System.out.println(tmp.getPriority());
+	//tmp.stop();
+
+/*
 	MThread t1=new MThread(); t1.setName("AA");
 	MThread t2=new MThread(); t2.setName("BB");
 	MThread t3=new MThread(); t3.setName("CC");
+
+	//t1.setPriority(Thread.MAX_PRIORITY);
+	//t2.setPriority(Thread.MIN_PRIORITY);
+	//t3.setPriority(Thread.NORM_PRIORITY);
+
+	//System.out.println(t1.getPriority());
+	//System.out.println(t2.getPriority());
+	//System.out.println(t3.getPriority());
+
 	t1.start();
 	t2.start();
 	t3.start();
+	//t2.join();
+	t2.join(25000);
+
+
+	if(t2.isAlive())
+	{
+	System.out.println("Hello BB");
+	}
+	else
+	{
+	System.out.println("Goodbye BB");
+	}
+
+*/
+
+
+
 	System.out.println("End-of-main");
 
 }
 }
 /*
+	void run()
+	void start()
+	void stop()
+	void setName(String)
+	String getName()
+	void setPriority(int)
+	int getPriority()
+	static void sleep(long)
+	void join()
+	void join(long timeOutPeriod)
+	boolean isAlive()
+	static Thread currentThread()
+
+	//returns the running thread object.
+
+
+	
+	
+
+
+
+
 	1) extends Thread class.
 	2) override the run method.
 	3) create thread objects.
